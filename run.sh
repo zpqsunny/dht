cb04db22bc940a1b16da20ad103f8a9c04c59246
zpq <zpqsunny@live.com>
2023-01-29 13:07:46 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,21 @@
 
 echo '##########################################'
 echo '#### 0: 检查环境 Docker docker-compose  ###'
-echo '#### 1: 初始化   Elasticsearch       ###'
-echo '#### 1: 初始化   mongodb       ###'
+echo '#### 1: 初始化   Elasticsearch          ###'
+echo '#### 2: 初始化   mongodb                ###'
 initElasticsearch() {
-  echo "initElasticsearch"
+  echo -e "\033[32m start Elasticsearch \033[0m"
   docker-compose up -d elasticsearch
-  # elasticsearch install plugin analysis
-  docker exec -it elasticsearch bash
-  elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.17.7/elasticsearch-analysis-ik-7.17.7.zip
-  echo -e "y\n"
+  echo -e "\033[32m elasticsearch install plugin analysis \033[0m"
+  /usr/bin/expect <<EOF
+  spawn exec -it elasticsearch bash
+  exec sleep 1
+  send "elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.17.7/elasticsearch-analysis-ik-7.17.7.zip \n"
+  expect "*n"
+  send "y\n"
+  expect "100%"
+  send "exit\n"
+EOF
   docker-compose restart elasticsearch
   echo "drop index if exits"
   curl -H "Content-Type: application/json" -X DELETE -d '' -u elastic:elastic http://127.0.0.1:9200/metadata
@@ -24,38 +30,64 @@ initElasticsearch() {
   exit
 }
 checkSystem() {
-  # check docker
-  if [ $(docker --version | grep -c "Docker") -ne 1 ];
-  then
-     yum install docker -y
+
+  if [ $(which expect | grep -c "expect") -ne 1 ]; then
+      yum install expect -y
   fi
-  echo -e "\033[31m Docker ok \033[0m";
-  if [ $(docker-compose -v | grep -c "Docker Compose") -ne 1 ];
-  then
+  
+  # check docker
+  if [ $(docker --version | grep -c "Docker") -ne 1 ]; then
+     yum install docker -y
+     systemctl enable docker
+     systemctl start docker
+  fi
+  echo -e "\033[32m Docker ok \033[0m";
+  if [ $(docker-compose -v | grep -c "Docker Compose") -ne 1 ]; then
     curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   fi
-  echo -e "\033[31m Docker Compose ok \033[0m";
+  echo -e "\033[32m Docker Compose ok \033[0m";
   if [ $(ls -l | grep -c "docker-compose.yml") -ne 1 ]; then
     wget https://raw.githubusercontent.com/zpqsunny/dht/main/docker-compose.yml
   fi
-  if [ $(ls -l | grep -c "mapping.json") -ne 1 ];
-    then
+  if [ $(ls -l | grep -c "mapping.json") -ne 1 ]; then
     wget https://raw.githubusercontent.com/zpqsunny/dht/main/mapping.json
   fi
-  echo -e "\033[31m mapping.json ok \033[0m";
-  if [ $(ls -l | grep -c "setting.json") -ne 1 ];
-  then
+  echo -e "\033[32m mapping.json ok \033[0m";
+  if [ $(ls -l | grep -c "setting.json") -ne 1 ]; then
     wget https://raw.githubusercontent.com/zpqsunny/dht/main/setting.json
-  echo -e "\033[31m setting.json ok \033[0m";
+  echo -e "\033[32m setting.json ok \033[0m";
   fi
+}
+initMongoDB() {
+  if [ $(docker ps -a | grep -c "mongo") -eq 1];
+   then
+    docker rm -f mongo
+    rm -rf /docker/mongo
+  fi
+  docker-compose up -d mongo
+  sleep 5
+  # init mongo
+  /usr/bin/expect <<EOF
+  spawn docker exec -it mongo bash
+  exec sleep 1
+  send "mongo --host 127.0.0.1:27018 -u \\\$MONGO_INITDB_ROOT_USERNAME -p \\\$MONGO_INITDB_ROOT_PASSWORD \n"
+  exec sleep 1
+  send "rs.initiate()\n"
+  exec sleep 1
+  send "exit\n"
+  exec sleep 1
+  send "exit\n"
+EOF
 }
 read c
 case $c in
   0) checkSystem
     ;;
   1) initElasticsearch
+    ;;
+  2) initMongoDB
     ;;
   *) echo "fail"
     ;;
@@ -110,6 +142,6 @@ docker-compose logs -f --tail 20 dht-peer dht-es
 firewall-cmd --add-port 6881/udp --permanent
 firewall-cmd --add-port 6882/udp --permanent
 firewall-cmd --add-port 6883/udp --permanent
-firewall-cmd --add-port 9200/tcp --permanent
+firewall-cmd --add-port 721/tcp --permanent
 firewall-cmd --add-port 9300/tcp --permanent
 firewall-cmd --reload

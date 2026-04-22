fcf700f9207e59006cb718ed2fc785072c58b6d1
zpq <zpqsunny@live.com>
2026-04-22 10:28:33 +0800

    fix: mongodb

--- dht-server/src/test/java/me/zpq/server/test/ServerTest.java
+++ dht-server/src/test/java/me/zpq/server/test/ServerTest.java
@@ -1,8 +1,6 @@
 package me.zpq.server.test;
 
 import lombok.extern.slf4j.Slf4j;
-import me.zpq.dht.common.PeerNode;
-import me.zpq.server.MemoryQueueImpl;
 import org.junit.Test;
 
 /**
@@ -14,19 +12,6 @@ public class ServerTest {
 
     @Test
     public void test() {
-        MemoryQueueImpl memoryQueue = new MemoryQueueImpl();
-//        memoryQueue.leftPush(new PeerNode("a","1",1,System.currentTimeMillis()));
-//        memoryQueue.leftPush(new PeerNode("b","2",2,System.currentTimeMillis()));
-//        memoryQueue.leftPush(new PeerNode("c","3",3,System.currentTimeMillis()));
-//        memoryQueue.leftPush(new PeerNode("c","3",3,System.currentTimeMillis()));
-//        memoryQueue.leftPush(new PeerNode("d","4",4,System.currentTimeMillis()));
-//        memoryQueue.leftPush(new PeerNode("e","5",5,System.currentTimeMillis()));
-        System.out.println(memoryQueue.size());
-        System.out.println(memoryQueue.rightPop());
-        System.out.println(memoryQueue.rightPop());
-        System.out.println(memoryQueue.rightPop());
-        System.out.println(memoryQueue.rightPop());
-        System.out.println(memoryQueue.rightPop());
-        System.out.println(memoryQueue.rightPop());
+
     }
 }
